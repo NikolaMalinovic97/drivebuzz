c8bd8067e24c66a320798f90359be665ffeac6ca
Nikola Malinovic <nikolamalinovic97@gmail.com>
2019-08-01 16:03:16 +0200

    Implemented OfferService.

---
+++ src/main/java/com/drivebuzz/demo/service/OfferServiceImpl.java
@@ -0,0 +1,63 @@
+package com.drivebuzz.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.drivebuzz.demo.dao.OfferDAO;
+import com.drivebuzz.demo.entity.Offer;
+
+@Service
+public class OfferServiceImpl implements OfferService {
+
+	@SuppressWarnings("unused")
+	private OfferDAO offerDAO;
+	
+	public OfferServiceImpl(OfferDAO offerDAO) {
+		this.offerDAO = offerDAO;
+	}
+
+	@Override
+	@Transactional
+	public List<Offer> findAll() {
+		return offerDAO.findAll();
+	}
+
+	@Override
+	@Transactional
+	public List<Offer> findAllActive() {
+		return offerDAO.findAllActive();
+	}
+
+	@Override
+	@Transactional
+	public List<Offer> findAllInactive() {
+		return offerDAO.findAllInactive();
+	}
+
+	@Override
+	@Transactional
+	public Offer findById(int theId) {
+		return offerDAO.findById(theId);
+	}
+
+	@Override
+	@Transactional
+	public List<Offer> findOffersForSpecificUser(int theId) {
+		return offerDAO.findOffersForSpecificUser(theId);
+	}
+
+	@Override
+	@Transactional
+	public void save(Offer theOffer) {
+		offerDAO.save(theOffer);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int theId) {
+		offerDAO.deleteById(theId);
+	}
+
+}
