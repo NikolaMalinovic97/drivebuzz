b1d5f5f19ee7073680eda6685f147fa840f0ec8d
Nikola Malinovic <nikolamalinovic97@gmail.com>
2019-08-08 18:25:03 +0200

    Added 'DemandDAO' interface.

---
+++ src/main/java/com/drivebuzz/demo/dao/DemandDAO.java
@@ -0,0 +1,22 @@
+package com.drivebuzz.demo.dao;
+
+import java.util.List;
+
+import com.drivebuzz.demo.entity.Demand;
+
+public interface DemandDAO {
+
+	public List<Demand> findAll();
+	
+	public List<Demand> findAllActive();
+	
+	public List<Demand> findAllInactive();
+	
+	public Demand findById(int theId);
+	
+	public List<Demand> findDemandsForSpecificUser(int theId);
+	
+	public void save(Demand theDemand);
+	
+	public void deleteById(int theId);	
+}
